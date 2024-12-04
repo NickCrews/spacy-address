b999c60d6664f1700b0c4230b9dff4e487ec8101
Nick Crews <nicholas.b.crews@gmail.com>
2024-12-04 19:05:36 +0900

    data: add some more data from chatgpt

--- eval.py
+++ eval.py
@@ -14,18 +14,34 @@ from spacy.training import Example
 from spacy_streamlit import visualize_ner
 
 
-def load_examples(nlp, dev_set):
-    doc_bin = DocBin().from_disk(dev_set)
-    docs = doc_bin.get_docs(nlp.vocab)
-    return [Example(nlp(reference.text), reference) for reference in docs]
+def load_examples(
+    nlp: spacy.Language | Path | None = None,
+    doc_bin: DocBin | Path | None = None,
+) -> list[Example]:
+    if nlp is None:
+        nlp = Path("./models/sm/training/model-last")
+    if isinstance(nlp, Path):
+        nlp = spacy.load(nlp)
+
+    if doc_bin is None:
+        doc_bin = Path("./data/dev.spacy")
+    if isinstance(doc_bin, Path):
+        doc_bin = DocBin().from_disk(doc_bin)
+
+    doc_bin = doc_bin.get_docs(nlp.vocab)
+    return [Example(nlp(reference.text), reference) for reference in doc_bin]
+
+
+def is_misprediction(example: Example) -> bool:
+    labels_ref = [ent.label_ for ent in example.reference.ents]
+    labels_pred = [ent.label_ for ent in example.predicted.ents]
+    return labels_ref != labels_pred
 
 
 def viz_examples(examples: Iterable[Example]):
     st.set_page_config(layout="wide")
     for i, ex in enumerate(examples):
-        labels_ref = [ent.label_ for ent in ex.reference.ents]
-        labels_pred = [ent.label_ for ent in ex.predicted.ents]
-        if labels_ref == labels_pred:
+        if not is_misprediction(ex):
             continue
         col1, col2 = st.columns(2)
         with col1:
@@ -44,12 +60,8 @@ def viz_examples(examples: Iterable[Example]):
             )
 
 
-def main(
-    nlp_dir: Path = "./models/sm/training/model-last",
-    dev_set: Path = "./data/dev.spacy",
-):
-    nlp = spacy.load(nlp_dir)
-    examples = load_examples(nlp, dev_set)
+def main(nlp: None | Path = None, docbin: None | Path = None):
+    examples = load_examples(nlp, docbin)
     viz_examples(examples)
 
 
